3e6db6069d7de2e6a0a6d21cd2fc4486fc567537
cwang1221 <wangcong891221@163.com>
2021-06-10 16:10:18 +0800

    chore: sign in page

--- src/pages/signInPage/SignInPage.js
+++ src/pages/signInPage/SignInPage.js
@@ -7,7 +7,7 @@ import { useAuth } from '../../contexts/authContext'
 import logo from '../../assets/images/logo.png'
 import { focusErrorInForm } from '../../utils'
 import { useDocumentTitle } from '../../hooks'
-import { MyLabel } from '../../components/myLabel/MyLabel'
+import { MyLabel } from '../../components'
 
 export function SignInPage() {
   const { t } = useTranslation()
@@ -28,44 +28,62 @@ export function SignInPage() {
   }
 
   return (
-    <SignInForm ref={formRef} labelCol={{ span: 24 }} wrapperCol={{ span: 24 }} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-      <Form.Item>
-        <Logo src={logo} preview={false} />
-      </Form.Item>
-      <Form.Item
-        label={<MyLabel required>{t('signIn.email')}</MyLabel>}
-        name="email"
-        rules={[{
-          required: true,
-          message: t('signIn.emailErrMsg')
-        }, {
-          type: 'email',
-          message: t('signIn.emailErrMsg')
-        }]}
-      >
-        <Input maxLength={100} />
-      </Form.Item>
-      <Form.Item
-        label={<MyLabel required>{t('signIn.password')}</MyLabel>}
-        name="password"
-        rules={[{
-          required: true,
-          message: t('signIn.passwordErrMsg')
-        }]}
-      >
-        <Input maxLength={100} type="password" />
-      </Form.Item>
-      <Form.Item style={{ marginTop: '3rem' }}>
-        <Button type="primary" htmlType="submit" style={{ width: '100%' }}>{t('signIn.signIn')}</Button>
-      </Form.Item>
-    </SignInForm>
+    <Container>
+      <Card>
+        <SignInForm ref={formRef} labelCol={{ span: 24 }} wrapperCol={{ span: 24 }} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+          <Form.Item>
+            <Logo src={logo} preview={false} />
+          </Form.Item>
+          <Form.Item
+            label={<MyLabel required>{t('signIn.email')}</MyLabel>}
+            name="email"
+            rules={[{
+              required: true,
+              message: t('signIn.emailErrMsg')
+            }, {
+              type: 'email',
+              message: t('signIn.emailErrMsg')
+            }]}
+          >
+            <Input maxLength={100} />
+          </Form.Item>
+          <Form.Item
+            label={<MyLabel required>{t('signIn.password')}</MyLabel>}
+            name="password"
+            rules={[{
+              required: true,
+              message: t('signIn.passwordErrMsg')
+            }]}
+          >
+            <Input maxLength={100} type="password" />
+          </Form.Item>
+          <Form.Item style={{ marginTop: '3rem' }}>
+            <Button type="primary" htmlType="submit" style={{ width: '100%' }}>{t('signIn.signIn')}</Button>
+          </Form.Item>
+        </SignInForm>
+      </Card>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 2rem;
+`
+
+const Card = styled.div`
+  border: 1px lightgray solid;
+  border-radius: 10px;
+  box-shadow: 5px 5px 5px lightgray;
+  padding: 3rem 2rem 4rem 2rem;
+`
 
 const SignInForm = styled(Form)`
   width: 20rem !important;
   margin: 0 auto !important;
-  padding-top: 5rem !important;
 `
 
 const Logo = styled(Image)`
